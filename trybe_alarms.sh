ea2a35f57f4c295fbf165e3f11e8364286410b92
matheusg18 <matheusgabriel.18@gmail.com>
2021-07-21 00:16:48 -0300

    Adicionei log e algumas funções

--- trybe_alarms.sh
+++ trybe_alarms.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+register_alarm()
+{
+    #$1 is the time, as hh:mm, when the notification will be shown
+    #$2 is the description
+    echo -e "Aviso de \"$2\" criado para as \"$1\"" >> ./logs/alarm_log.txt
+}
+
+remove_alarm_from_log()
+{
+    #must implement
+}
+
+show_log()
+{
+    while IFS= read -r line || [[ -n "$line" ]]
+        do
+            echo "================================================================"
+            echo -e "\t$line"
+        done < ./logs/alarm_log.txt
+
+    echo "================================================================"
+}
+
 add_alarm()
 {
     #$1 is the time, as hh:mm, when the notification will be shown
@@ -8,19 +31,23 @@ add_alarm()
     TARGET_TIME=$(date -d "$1" +%s)
     SLEEP_SECONDS=$(($TARGET_TIME - $CURRENT_TIME))
 
-    echo "================================================================"
     if [ $SLEEP_SECONDS -lt 0 ]
         then
-            echo -e "\tPassou do horário de \"$2\"!"
+            echo -e "---> Já passou do horário de \"$2\"!"
         else
             (sleep $SLEEP_SECONDS && notify-send -i ~/trybe_alarms/resources/betrybe.jpg -t 10 Trybe "$2") &
-            echo -e "\tAviso de \"$2\" criado para as \"$1\""
+            register_alarm "$1" "$2"
     fi
 }
 
 #Flux control variables
 i=0
 TRYBE_HOUR=""
+
+if [ $# -eq 0 ]
+    then
+        show_log
+fi
 
 #Main loop
 for arg in "$@"
@@ -34,4 +61,3 @@ for arg in "$@"
                 i=0
         fi
     done
-echo "================================================================"
